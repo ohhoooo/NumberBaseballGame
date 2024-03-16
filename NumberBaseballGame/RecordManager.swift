1465ae96c7253bf0ce9ebcb19c407dbb3d8e7665
KJH <gh7052@gmail.com>
2024-03-16 23:54:35 +0900

    Fix : Lv5 조회 시 게임 기록이 없는 경우 예외 처리 추가

--- NumberBaseballGame/RecordManager.swift
+++ NumberBaseballGame/RecordManager.swift
@@ -17,10 +17,14 @@ final class RecordManager {
     }
     
     func showRecords() {
-        print("< 게임 기록 보기 >")
-        
-        for index in 0...games.count-1 {
-            print("\(index+1)번째 게임 : 시도 횟수 - \(games[index])")
+        if games.isEmpty {
+            print("아직 게임 기록이 없습니다. 새로운 게임을 시작하여 기록을 만들어보세요!")
+        } else {
+            print("< 게임 기록 보기 >")
+            
+            for index in 0...games.count-1 {
+                print("\(index+1)번째 게임 : 시도 횟수 - \(games[index])")
+            }
         }
     }
 }
